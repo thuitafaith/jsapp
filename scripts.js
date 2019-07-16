bac45f7f5f76271f79051fbb83985f346aa1cbde
thuitafaith <thuitamuthoni15@gmail.com>
2019-07-16 11:45:24 +0300

    create event listeners function

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function eventListeners(){
+    const budgetForm = document.getElementById("budget-form")
+    const budgetInput = document.getElementById("budget-input")
+    const budgetSubmit = document.getElementById("budget-submit")
+} 
